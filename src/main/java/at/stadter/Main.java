041d4efe02a2f7bfc10bc72a99c755e4f777a571
Philippovitsch <philipp@stadter.at>
2023-12-14 13:17:12 +0100

    Implement main function to start application

--- src/main/java/at/stadter/Main.java
+++ src/main/java/at/stadter/Main.java
@@ -1,9 +1,20 @@
 package at.stadter;
 
+import at.stadter.askomat.AskOMatController;
+import at.stadter.askomat.AskOMatView;
+import at.stadter.askomat.QuestionRepository;
+import at.stadter.askomat.UserInputHandler;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserInputHandler inputHandler = new UserInputHandler();
+        AskOMatView askOMatView = new AskOMatView();
+        QuestionRepository questionRepository = new QuestionRepository();
+
+        AskOMatController askOMat = new AskOMatController(inputHandler, askOMatView, questionRepository);
+
+        askOMat.start();
     }
 
 }
