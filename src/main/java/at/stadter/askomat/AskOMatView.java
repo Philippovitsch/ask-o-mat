0f66f69680f60862fe6d6dc567d81eb72c031193
Philippovitsch <philipp@stadter.at>
2023-12-14 12:50:19 +0100

    Implement basic functionality of view

--- src/main/java/at/stadter/askomat/AskOMatView.java
+++ src/main/java/at/stadter/askomat/AskOMatView.java
@@ -2,4 +2,28 @@ package at.stadter.askomat;
 
 public class AskOMatView {
 
+    public void printWelcomeMessage() {
+        System.out.println("╔════════════════════════╗");
+        System.out.println("║  Welcome to ASK-O-MAT  ║");
+        System.out.println("╚════════════════════════╝");
+    }
+
+    public void printMainMenu() {
+        System.out.println("Please choose an option:");
+        System.out.println("-> 1    Ask a question");
+        System.out.println("-> 2    Add a question");
+        System.out.println("-> Q    Exit");
+        System.out.print("Option: ");
+    }
+
+    public void printAnswers(QuestionModel question) {
+        for (String answer : question.answers()) {
+            System.out.println("- " + answer);
+        }
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
 }
