7963a612c6b731462014fc8c370cd2d953c1d71e
Philippovitsch <philipp@stadter.at>
2023-12-14 16:23:27 +0100

    Implement feature to add questions

--- src/main/java/at/stadter/askomat/AskOMatController.java
+++ src/main/java/at/stadter/askomat/AskOMatController.java
@@ -37,7 +37,18 @@ public class AskOMatController {
     }
 
     private void addQuestion() {
-
+        askOMatView.printMessage("Please enter your question in the following format:");
+        askOMatView.printMessage("<question>? \"<answer1>\" \"<answer2>\" \"<answerX>\"");
+        String userQuestion = inputHandler.getUserInput();
+        if (!questionService.isValidFormat(userQuestion)) {
+            askOMatView.printMessage("Wrong format - try again!");
+        } else if (!questionService.isWithinMaxLength(userQuestion)) {
+            askOMatView.printMessage("Your question/ answer is too long - try again!");
+        } else {
+            QuestionModel question = questionService.createQuestion(userQuestion);
+            questionService.addQuestion(question);
+            askOMatView.printMessage("Your questions has been saved!");
+        }
     }
 
 }
--- src/main/java/at/stadter/askomat/QuestionService.java
+++ src/main/java/at/stadter/askomat/QuestionService.java
@@ -1,11 +1,59 @@
 package at.stadter.askomat;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class QuestionService {
+
+    private final String SEPARATOR = "?";
+    private final String DELIMITER = "\"";
+    private final String SPACE = " ";
+    private final int MAX_LENGTH = 255;
 
     private final QuestionRepository questionRepository;
 
     public QuestionService(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
+    }
+
+    public boolean isValidFormat(String userQuestion) {
+        if (!userQuestion.contains(SEPARATOR) || userQuestion.startsWith(SEPARATOR) || userQuestion.endsWith(SEPARATOR)) {
+            return false;
+        }
+        String rawAnswers = userQuestion.substring(userQuestion.indexOf(SEPARATOR) + 1);
+        return  rawAnswers.startsWith(SPACE) &&
+                Arrays.stream(rawAnswers.split(SPACE))
+                    .filter(answer -> !answer.isBlank())
+                    .allMatch(answer ->
+                            answer.startsWith(DELIMITER) &&
+                            answer.endsWith(DELIMITER) &&
+                            answer.length() > 2 &&
+                            answer.chars().filter(character -> character == DELIMITER.charAt(0)).count() == 2
+                    );
+    }
+
+    public boolean isWithinMaxLength(String userQuestion) {
+        String question = userQuestion.substring(0, userQuestion.indexOf(SEPARATOR) + 1);
+        String rawAnswers = userQuestion.substring(userQuestion.indexOf(SEPARATOR) + 1);
+        return  question.length() <= MAX_LENGTH &&
+                Arrays.stream(rawAnswers.split(SPACE))
+                    .filter(answer -> !answer.isBlank())
+                    .map(answer -> answer.substring(1, answer.length() - 1))
+                    .allMatch(answer -> answer.length() <= MAX_LENGTH);
+    }
+
+    public QuestionModel createQuestion(String userQuestion) {
+        String question = userQuestion.substring(0, userQuestion.indexOf(SEPARATOR) + 1);
+        String rawAnswers = userQuestion.substring(userQuestion.indexOf(SEPARATOR) + 1);
+        List<String> answers = Arrays.stream(rawAnswers.split(SPACE))
+                .filter(answer -> !answer.isBlank())
+                .map(answer -> answer.substring(1, answer.length() - 1))
+                .toList();
+        return new QuestionModel(question, answers);
+    }
+
+    public void addQuestion(QuestionModel question) {
+        questionRepository.addQuestion(question);
     }
 
 }
