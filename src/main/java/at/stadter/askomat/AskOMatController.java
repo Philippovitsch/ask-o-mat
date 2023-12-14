f67238695d877bf83fd1da0e4aecded7d2497e9c
Philippovitsch <philipp@stadter.at>
2023-12-14 13:16:19 +0100

    Implement main logic to run the application

--- src/main/java/at/stadter/askomat/AskOMatController.java
+++ src/main/java/at/stadter/askomat/AskOMatController.java
@@ -2,4 +2,42 @@ package at.stadter.askomat;
 
 public class AskOMatController {
 
+    private final UserInputHandler inputHandler;
+    private final AskOMatView askOMatView;
+    private final QuestionRepository questionRepository;
+
+    public AskOMatController(
+            UserInputHandler inputHandler,
+            AskOMatView askOMatView,
+            QuestionRepository questionRepository
+    ) {
+        this.questionRepository = questionRepository;
+        this.askOMatView = askOMatView;
+        this.inputHandler = inputHandler;
+    }
+
+    public void start() {
+        askOMatView.printWelcomeMessage();
+        boolean isRunning = true;
+        while (isRunning) {
+            askOMatView.printMainMenu();
+            String option = inputHandler.getUserInput();
+            switch (option) {
+                case "1" -> askQuestion();
+                case "2" -> addQuestion();
+                case "q", "Q" -> isRunning = false;
+                default -> askOMatView.printMessage("Unknown option - try again!");
+            }
+        }
+        askOMatView.printMessage("Bye bye!");
+    }
+
+    private void askQuestion() {
+
+    }
+
+    private void addQuestion() {
+
+    }
+
 }
